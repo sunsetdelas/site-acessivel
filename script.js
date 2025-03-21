6fa790ba5f505b9e4beb77f3db506fd082aadb00
sunsetdelas <138136444+sunsetdelas@users.noreply.github.com>
2025-03-21 11:36:20 -0300

    botao-acessivel

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
  
+     
      const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
      botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
    
